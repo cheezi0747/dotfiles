72a2f235d49df18abaf21b219033672c653f7f01
cheezi0747 <linus.sjolinder@b3.se>
2024-12-06 17:09:07 +0100

    stuff

--- .github/install.sh
+++ .github/install.sh
@@ -16,14 +16,18 @@ clone_dotfiles_repo() {
 install_xcode() {
     echo "Checking Command Line Tools for Xcode"
     # Only run if the tools are not installed yet
-    # To check that try to print the SDK path
-    xcode-select -p &>/dev/null
-    if [ $? -ne 0 ]; then
+    if ! xcode-select -p &>/dev/null; then
         echo "Command Line Tools for Xcode not found. Installing from softwareupdate…"
         # This temporary file prompts the 'softwareupdate' utility to list the Command Line Tools
         touch /tmp/.com.apple.dt.CommandLineTools.installondemand.in-progress
         PROD=$(softwareupdate -l | grep "\*.*Command Line" | tail -n 1 | sed 's/^[^C]* //')
-        softwareupdate -i "$PROD" --verbose
+        if [ -n "$PROD" ]; then
+            softwareupdate -i "$PROD" --verbose
+            echo "Command Line Tools for Xcode installed successfully."
+        else
+            echo "No Command Line Tools found for installation. Please check your internet connection or try manually."
+        fi
+        rm /tmp/.com.apple.dt.CommandLineTools.installondemand.in-progress
     else
         echo "Command Line Tools for Xcode have been installed."
     fi
